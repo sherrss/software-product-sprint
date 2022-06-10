ee6960665c09b7645d396c161a8cd7945a595d91
Sherry Liu <liu1500sherry@gmail.com>
2022-06-10 03:18:50 +0000

    Modify Sample Servlet to return ArrayList of messages

--- portfolio/src/main/java/com/google/sps/servlets/SampleServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/SampleServlet.java
@@ -7,13 +7,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
 @WebServlet("/sample")
 public class SampleServlet extends HttpServlet {
+
+    ArrayList<String> messages = new ArrayList<String>(Arrays.asList("To infinity and beyond!", "May the force be with you", "Im the king of the world!"));
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("Hardcoded Time: June 9th, 2022 @10:30pm");
+
+    Gson gson = new Gson();
+    String json = gson.toJson(messages);
+    
+    response.getWriter().println(json);
   }
     
 }
