4706c2d188a68b7142634d9ffc8693aa7061a07e
Sherry Liu <liu1500sherry@gmail.com>
2022-06-13 21:38:26 +0000

    Create an entity with properties of entered text value and timestamp

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
 import com.google.cloud.datastore.KeyFactory;
 
 @WebServlet("/form-handler")
@@ -18,15 +20,24 @@ public class FormHandlerServlet extends HttpServlet {
 
     // Get the value entered in the form.
     String textValue = request.getParameter("text-input");
+    // Get the time of when the value is entered. 
+    long timestamp = System.currentTimeMillis();
 
     // Print the value so you can see it in the server logs.
     System.out.println("You submitted: " + textValue);
 
-    // Create instance of Datastore 
+    // Create instance of Datastore.
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 
-    // Create key with a king of "Task"
+    // Create key with a kind of "Task".
     KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task");
+
+    // Create entity with properties of text value and timestamp.
+    FullEntity taskEntity =
+    Entity.newBuilder(keyFactory.newKey())
+        .set("value", textValue)
+        .set("timestamp", timestamp)
+        .build();
 
     //Redirect user back to portfolio page
     response.sendRedirect("http://sliu-sps-summer22.appspot.com ");
